fbb37990b73790ca2be80cc34383de17280e2bad
jwdietrich <jwdietrich@81b5e34f-84f8-425d-b519-8732f0cf0997>
2019-01-31 19:28:47 +0000

    Adding additional test cases for RPN Engine.

--- trunk/Unit Tests of RPN Engine/rpnenginetestcases.pas
+++ trunk/Unit Tests of RPN Engine/rpnenginetestcases.pas
@@ -38,7 +38,58 @@ type
     procedure CodeVersionCheck;
   end;
 
+  { TEngineFunctionTestCases }
+
+  TEngineFunctionTestCases = class(TTestCase)
+  published
+    procedure UnaryFuntionTests;
+    procedure BinaryFunctionTests;
+    procedure CompoundFunctionTests;
+  end;
+
 implementation
+
+{ TEngineFunctionTestCases }
+
+procedure TEngineFunctionTestCases.UnaryFuntionTests;
+var
+  TestEngine: TEngine;
+begin
+  TestEngine := TEngine.create;
+  TestEngine.Stack := TStack.create;
+  AssertEquals(-1, TestEngine.rpn(1, PlusMinusOp));
+  AssertEquals(1, TestEngine.rpn(-1, PlusMinusOp));
+  AssertEquals(0.2, TestEngine.rpn(5, InvertOp));
+  AssertEquals(4, TestEngine.rpn(16, sqrtOp));
+  TestEngine.destroy;
+end;
+
+procedure TEngineFunctionTestCases.BinaryFunctionTests;
+var
+  TestEngine: TEngine;
+begin
+  TestEngine := TEngine.create;
+  TestEngine.Stack := TStack.create;
+  AssertEquals(1, TestEngine.rpn(-4, 5, PlusOp));
+  AssertEquals(7, TestEngine.rpn(13, 6, MinusOp));
+  AssertEquals(24, TestEngine.rpn(3, 8, MultOp));
+  AssertEquals(2.5, TestEngine.rpn(5, 2, DivOp));
+  AssertEquals(2, TestEngine.rpn(5, 2.5, DivOp));
+  AssertEquals(32, TestEngine.rpn(2, 5, PowerOp));
+  TestEngine.destroy;
+end;
+
+procedure TEngineFunctionTestCases.CompoundFunctionTests;
+var
+  TestEngine: TEngine;
+begin
+  TestEngine := TEngine.create;
+  TestEngine.Stack := TStack.create;
+  AssertEquals(0.8, TestEngine.rpn(TestEngine.rpn(2, 2, PlusOp), 5, DivOp));
+  AssertEquals(14, TestEngine.rpn(5, TestEngine.rpn(TestEngine.rpn(TestEngine.rpn(1, 2, PlusOp), 4, MultOp), 3, MinusOp), PlusOp));
+  AssertEquals(2, TestEngine.rpn(TestEngine.rpn(4, TestEngine.rpn(2, 5, MultOp), PlusOp), TestEngine.rpn(1, TestEngine.rpn(3, 2, MultOp), PlusOp), DivOp));
+  TestEngine.destroy;
+end;
 
 { TControlTestCases }
 
@@ -58,6 +109,7 @@ end;
 initialization
 
 RegisterTest(TControlTestCases);
+RegisterTest(TEngineFunctionTestCases);
 
 end.
 
