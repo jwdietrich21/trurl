2d80fc1a13d09f171432e6922cdc0d8a7f954cf2
jwdietrich <jwdietrich@81b5e34f-84f8-425d-b519-8732f0cf0997>
2019-07-06 18:25:55 +0000

    Consistent versioning.

--- rpnengine.pas
+++ rpnengine.pas
@@ -6,7 +6,7 @@ unit RPNEngine;
 
 { Basic RPN Engine }
 
-{ Version 1.0 (Bet) }
+{ Version 2.0 (Bet) }
 
 { (c) Johannes W. Dietrich, 2003 - 2019 }
 
@@ -40,10 +40,10 @@ const
   RPNEngine_major   = 2;
   RPNEngine_minor   = 0;
   RPNEngine_release = 0;
-  RPNEngine_patch   = 93;
+  RPNEngine_patch   = 94;
   RPNEngine_fullversion = ((RPNEngine_major * 100 + RPNEngine_minor) *
     100 + RPNEngine_release) * 100 + RPNEngine_patch;
-  RPNEngine_version = '2.0.0.93';
+  RPNEngine_version = '2.0.0.94';
   RPNEngine_internalversion = 'Bet';
 
 type
